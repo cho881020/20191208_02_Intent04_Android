049eecd4387dec90af090f1472aef794e353476c
조경진 <kj_cho@idealidea.co.kr>
2019-12-08 12:30:13 +0900

    카카오톡 플레이스토어 링크

--- app/src/main/java/kr/co/tjoeun/a20191208_02_intent04_android/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/a20191208_02_intent04_android/MainActivity.kt
@@ -47,6 +47,13 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        kakaoAppBtn.setOnClickListener {
+//            deatils? 부터는 플레이스토어 링크를 검색해서 붙여넣기.
+            val uri = Uri.parse("market://details?id=com.kakao.talk&hl=ko")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
 
     }
 }
